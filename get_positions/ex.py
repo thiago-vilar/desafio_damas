c245aeafd82f176545601780a9851d130efa4972
thiago-vilar <tcv3@softex.cin.ufpe.br>
2024-08-23 16:36:58 -0300

    pictures

--- get_positions/ex.py
+++
@@ -1,9 +0,0 @@
-from rria_api.robot_enum import RobotEnum
-from rria_api.robot_object import RobotObject
-
-
-robot = RobotObject('192.168.2.10', RobotEnum.GEN3_LITE)
-robot.connect_robot()
-
-robot.move_joints(20.665, 350.727, 97.484, 267.707, 287.288, 115.2) # 'webcam_view'
-
